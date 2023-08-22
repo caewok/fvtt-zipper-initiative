85c2ebbc7552cec157b0248f8b2b5617652bfdf4
Michael Enion <caewok_2000@yahoo.com>
2023-08-22 09:08:13 -0700

    Add setting to choose NPC leader by initiative roll

--- scripts/settings.js
+++ scripts/settings.js
@@ -8,7 +8,8 @@ import { MODULE_ID } from "./const.js";
 
 export const SETTINGS = {
   CHANGELOG: "changelog",
-  RESET_EACH_ROUND: "resetEachRound"
+  RESET_EACH_ROUND: "resetEachRound",
+  NPC_LEADER_HIGHEST_INIT: "npcLeaderHighestInitiative"
 };
 
 export function getSetting(settingName) {
@@ -28,6 +29,15 @@ export function registerSettings() {
   game.settings.register(MODULE_ID, SETTINGS.RESET_EACH_ROUND, {
     name: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.RESET_EACH_ROUND}.Name`),
     hint: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.RESET_EACH_ROUND}.Hint`),
+    type: Boolean,
+    default: false,
+    scope: "world",
+    config: true
+  });
+
+  game.settings.register(MODULE_ID, SETTINGS.NPC_LEADER_HIGHEST_INIT, {
+    name: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.NPC_LEADER_HIGHEST_INIT}.Name`),
+    hint: game.i18n.localize(`${MODULE_ID}.settings.${SETTINGS.NPC_LEADER_HIGHEST_INIT}.Hint`),
     type: Boolean,
     default: false,
     scope: "world",
